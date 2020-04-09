af9ce6c466f2a6a1b2bc63a613dca0fe4d2cdc64
Ariel Carrera <cesar.carrera@pjn.gov.ar>
2020-04-09 20:15:56 -0300

    Added suffixes to centos / ubuntu files

--- docker-graalvm/centos/build.sh
+++ docker-graalvm/centos/build.sh
@@ -2,7 +2,7 @@
 
 $JAVA_HOME/bin/native-image --no-server \
          --class-path /project/target/environment-var-replacer.jar \
-	     -H:Name=environment-var-replacer \
+	     -H:Name=environment-var-replacer-centos \
 	     -H:Class=com.github.arielcarrera.env.var.replacer.EnvVarReplacer \
 	     -H:+ReportUnsupportedElementsAtRuntime \
 	     -H:+AllowVMInspection
